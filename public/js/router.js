f1c07552375901c17fad00d7858e676a8ea6729a
Mars <max.rez@list.ru>
2021-05-10 02:03:35 +0300

    html fixes

--- public/js/router.js
+++ public/js/router.js
@@ -13,12 +13,12 @@ const possibleHash = [
 async function getHtml(viewName) {
   switch (document.documentElement.lang) {
     case "ru": {
-      const resp = await fetch(`../views/ru/${viewName}.ru.html`);
+      const resp = await fetch(`views/ru/${viewName}.ru.html`);
       const html = await resp.text();
       return html;
     }
     case "en": {
-      const resp = await fetch(`../views/${viewName}.html`);
+      const resp = await fetch(`views/${viewName}.html`);
       const html = await resp.text();
       return html;
     }
